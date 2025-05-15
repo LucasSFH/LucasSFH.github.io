b317e60f12215c699c5552c738008f97ea57f3db
LucasFriis <lucasfriis123@gmail.com>
2025-05-15 08:47:51 +0200

    updated info and layout to case card

--- utile-solutions/src/Pages/CasesPage.tsx
+++ utile-solutions/src/Pages/CasesPage.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "antd";
-import { useNavigate, Link } from "react-router-dom";
+import {BulbFilled} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 import AllCases from "../Data/Cases/AllCases";
 
 const { Meta } = Card;
 
 const CasesPage = () => {
-  const navigate = useNavigate();
+  const [clickedIndex, setClickedIndex] = useState<number | null>(null);
 
   const casesArray = AllCases();
 
@@ -15,11 +16,21 @@ const CasesPage = () => {
       <h1 className="mb-8 text-5xl font-bold">Cases</h1>
 
       {casesArray.map((item, index) => (
-        <Link to={item.websiteUrl}  className="max-w-xl w-full" key={index} target="_blank">
+        <Link to={item.websiteUrl}  className="max-w-xl w-full" key={index} target="_blank"
+          onMouseOver={() => setClickedIndex(index)}
+          onMouseLeave={() => setClickedIndex(null)}
+        >
           <Card key={index} className="shadow-md hover:shadow-lg hover:opacity-85"
             cover={<img alt={item.projectName} src={item.backgroundImage} />}
           >
-            <Meta title={item.projectName} description={item.shortDescription} />
+            <Meta title={item.projectName} description={
+              <>
+                <p className="hidden sm:block">{clickedIndex === index ? (item.description !== "" ? item.description : item.shortDescription) : <>{item.shortDescription}..</>}</p>
+                <p className="block sm:hidden">{item.shortDescription}TEST</p>
+                
+                <p><BulbFilled className="text-yellow-400 mr-2 mt-4" /><i><b>{item.technologyInfo}</b></i></p>     
+              </>
+              } />
           </Card>
         </Link>
         
